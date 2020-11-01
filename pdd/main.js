261aff82cd7dc95f2624c7b6f38866d7ffd44c75
sultanbayev <sultanbayev@gmail.com>
2020-11-01 15:36:07 +0600

    update pdd

--- pdd/main.js
+++ pdd/main.js
@@ -1,4 +1,126 @@
-const style = [{"elementType":"geometry","stylers":[{"color":"#f5f5f5"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#f5f5f5"}]},{"featureType":"administrative.land_parcel","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.fill","stylers":[{"color":"#bdbdbd"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#dadada"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#c9c9c9"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]}];
+const style = [{
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#f5f5f5"
+    }]
+}, {
+    "elementType": "labels.icon",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}, {
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#616161"
+    }]
+}, {
+    "elementType": "labels.text.stroke",
+    "stylers": [{
+        "color": "#f5f5f5"
+    }]
+}, {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}, {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#bdbdbd"
+    }]
+}, {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#eeeeee"
+    }]
+}, {
+    "featureType": "poi",
+    "elementType": "labels.text",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}, {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#757575"
+    }]
+}, {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#e5e5e5"
+    }]
+}, {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#9e9e9e"
+    }]
+}, {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#ffffff"
+    }]
+}, {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#757575"
+    }]
+}, {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#dadada"
+    }]
+}, {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#616161"
+    }]
+}, {
+    "featureType": "road.local",
+    "elementType": "labels",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}, {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#9e9e9e"
+    }]
+}, {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#e5e5e5"
+    }]
+}, {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#eeeeee"
+    }]
+}, {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#c9c9c9"
+    }]
+}, {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#9e9e9e"
+    }]
+}];
 
 const almaty = {
     center: {
@@ -70,7 +192,7 @@ let request = {
     nauryzbay: "nauryzbay",
     turksib: "turksib",
     zhetsu: "zhetsu",
-    
+
     sober: "sober",
     unknown: "unknown",
     dui: "dui",
@@ -114,6 +236,43 @@ function setMapCenter() {
     }
 }
 
+const city = document.getElementById('city');
+city.addEventListener('change', (e) => {
+    if (e.target.value === 'НУР-СУЛТАН') {
+        disableDistricts();
+    } else if (e.target.value === 'АЛМАТЫ') {
+        enableDistricts();
+    }
+})
+
+function disableDistricts() {
+    const alatau = document.getElementById('alatau');
+    const almaly = document.getElementById('almaly');
+    const auezov = document.getElementById('auezov');
+    const bostandyk = document.getElementById('bostandyk');
+    const medeu = document.getElementById('medeu');
+    const nauryzbay = document.getElementById('nauryzbay');
+    const turksib = document.getElementById('turksib');
+    const zhetsu = document.getElementById('zhetsu');
+    [alatau, almaly, auezov, bostandyk, medeu, nauryzbay, turksib, zhetsu].forEach(checkbox => {
+        checkbox.disabled = true;
+    })
+}
+
+function enableDistricts() {
+    const alatau = document.getElementById('alatau');
+    const almaly = document.getElementById('almaly');
+    const auezov = document.getElementById('auezov');
+    const bostandyk = document.getElementById('bostandyk');
+    const medeu = document.getElementById('medeu');
+    const nauryzbay = document.getElementById('nauryzbay');
+    const turksib = document.getElementById('turksib');
+    const zhetsu = document.getElementById('zhetsu');
+    [alatau, almaly, auezov, bostandyk, medeu, nauryzbay, turksib, zhetsu].forEach(checkbox => {
+        checkbox.disabled = false;
+    })
+}
+
 function processRecords() {
     setMapCenter();
     const filename = 'query' + request.year + '.json';
@@ -123,15 +282,18 @@ function processRecords() {
         // const uniques = [new Set(records.map(record => record.attributes['FD1R141P1']))];
         // console.log(uniques)
         records = filterByCity(records, request.city);
-        records = filterByDistrict(records);
+        if (request.city === 'АЛМАТЫ') {
+            records = filterByDistrict(records);
+        }
         records = filterByType(records);
         records = filterByDaytime(records);
-        records = filterByCondition(records);
         records = filterByDeathsAndInjuries(records);
+        records = filterByCondition(records);
+
 
         updateStats(records);
         updateCircles(records);
-        
+
     });
 }
 
@@ -208,9 +370,6 @@ function filterByDaytime(records) {
 
 function filterByCondition(records) {
     let newRecords = records;
-    if (!request.unknown) {
-        newRecords = newRecords.filter(record => record.attributes['FD1R141P1'] !== null);
-    }
     if (!request.sober) {
         newRecords = newRecords.filter(record => record.attributes['FD1R141P1'] !== 'трезвый');
     }
@@ -220,10 +379,14 @@ function filterByCondition(records) {
         newRecords = newRecords.filter(record => record.attributes['FD1R141P1'] !== 'токсикоманическое опьянение');
     }
     if (!request.other) {
-        newRecords = newRecords.filter(record => record.attributes['FD1R141P1'] !== 'резкое ухудшение здоровья ');
-        newRecords = newRecords.filter(record => record.attributes['FD1R141P1'] !== 'время непрерывного нахождения в пути ');
+        newRecords = newRecords.filter(record => record.attributes['FD1R141P1'] !== 'резкое ухудшение здоровья');
+        newRecords = newRecords.filter(record => record.attributes['FD1R141P1'] !== "время непрерывного нахождения в пути");
+    }
+    if (!request.unknown) {
+        newRecords = newRecords.filter(record => record.attributes['FD1R141P1'] !== null);
     }
     return newRecords;
+
 }
 
 function filterByCity(records, cityName) {
@@ -267,11 +430,8 @@ function updateAccidentsStatsByDaytime(records) {
 function updateAccidentsStatsByCondition(records) {
     const sober = (records.filter(record => record.attributes['FD1R141P1'] === 'трезвый')).length;
     const unknown = (records.filter(record => record.attributes['FD1R141P1'] === null)).length;
-    const dui = (records.filter(record => record.attributes['FD1R141P1'] === 'алкогольное опьянение')).length
-        + (records.filter(record => record.attributes['FD1R141P1'] === 'наркотическое опьянение')).length
-        + (records.filter(record => record.attributes['FD1R141P1'] === 'токсикоманическое опьянение')).length;
-    const other = (records.filter(record => record.attributes['FD1R141P1'] === 'резкое ухудшение здоровья ')).length
-        + (records.filter(record => record.attributes['FD1R141P1'] === 'время непрерывного нахождения в пути ')).length;
+    const dui = records.filter(record => record.attributes['FD1R141P1'] === 'алкогольное опьянение' || record.attributes['FD1R141P1'] === 'наркотическое опьянение' || record.attributes['FD1R141P1'] === 'токсикоманическое опьянение').length;
+    const other = records.filter(record => record.attributes['FD1R141P1'] === 'резкое ухудшение здоровья' || record.attributes['FD1R141P1'] === 'время непрерывного нахождения в пути').length;
     const statsContainer = document.getElementById('stats');
     const thisStats = document.createElement('div');
     thisStats.classList.add('sub2');
@@ -280,7 +440,7 @@ function updateAccidentsStatsByCondition(records) {
 }
 
 function updateDeathsStats(records) {
-    const total = records.reduce( ((total, current) => { 
+    const total = records.reduce(((total, current) => {
         return total + current.attributes['FD1R13P2'];
     }), 0);
     const statsContainer = document.getElementById('stats');
@@ -290,7 +450,7 @@ function updateDeathsStats(records) {
 }
 
 function updateInjuriesStats(records) {
-    const total = records.reduce( ((total, current) => { 
+    const total = records.reduce(((total, current) => {
         return total + current.attributes['FD1R13P1'];
     }), 0);
     const statsContainer = document.getElementById('stats');
@@ -300,44 +460,43 @@ function updateInjuriesStats(records) {
 }
 
 
-    // const heatmap = new google.maps.visualization.HeatmapLayer({
-    //     data: features,
-    //     // radius: 30,
-    //     // maxIntensity: 10,
-    //     dissipating: true,
-    //     gradient: [
-    //         'rgba(0, 255, 255, 0)',
-    //         'rgba(0, 255, 255, 1)',
-    //         'rgba(0, 191, 255, 1)',
-    //         'rgba(0, 127, 255, 1)',
-    //         'rgba(0, 63, 255, 1)',
-    //         'rgba(0, 0, 255, 1)',
-    //         'rgba(0, 0, 223, 1)',
-    //         'rgba(0, 0, 191, 1)',
-    //         'rgba(0, 0, 159, 1)',
-    //         'rgba(0, 0, 127, 1)',
-    //         'rgba(63, 0, 91, 1)',
-    //         'rgba(127, 0, 63, 1)',
-    //         'rgba(191, 0, 31, 1)',
-    //         'rgba(255, 0, 0, 1)'
-    //     ]
-    // });
-
-    // heatmap.setMap(map);
-    // features.forEach(feature => {
-    //     if (feature.isDeath === 1) {
-    //         const circle = new google.maps.Circle({
-    //                 center: feature.location,
-    //                 map: map,
-    //                 radius: 50,
-    //                 clickable: false,
-    //                 fillOpacity: 0.8,
-    //                 fillColor: '#000000',
-    //                 strokeWeight: 0,
-    //                 strokeOpacity: 0,
-    //                 strokeColor: '#ff0000',
-    //             })
-    //     }
-    // });
+// const heatmap = new google.maps.visualization.HeatmapLayer({
+//     data: features,
+//     // radius: 30,
+//     // maxIntensity: 10,
+//     dissipating: true,
+//     gradient: [
+//         'rgba(0, 255, 255, 0)',
+//         'rgba(0, 255, 255, 1)',
+//         'rgba(0, 191, 255, 1)',
+//         'rgba(0, 127, 255, 1)',
+//         'rgba(0, 63, 255, 1)',
+//         'rgba(0, 0, 255, 1)',
+//         'rgba(0, 0, 223, 1)',
+//         'rgba(0, 0, 191, 1)',
+//         'rgba(0, 0, 159, 1)',
+//         'rgba(0, 0, 127, 1)',
+//         'rgba(63, 0, 91, 1)',
+//         'rgba(127, 0, 63, 1)',
+//         'rgba(191, 0, 31, 1)',
+//         'rgba(255, 0, 0, 1)'
+//     ]
 // });
 
+// heatmap.setMap(map);
+// features.forEach(feature => {
+//     if (feature.isDeath === 1) {
+//         const circle = new google.maps.Circle({
+//                 center: feature.location,
+//                 map: map,
+//                 radius: 50,
+//                 clickable: false,
+//                 fillOpacity: 0.8,
+//                 fillColor: '#000000',
+//                 strokeWeight: 0,
+//                 strokeOpacity: 0,
+//                 strokeColor: '#ff0000',
+//             })
+//     }
+// });
+// });
